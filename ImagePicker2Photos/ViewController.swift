2101f24905db69b96999fe1a1d388d426ae292dd
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-09-04 06:42:44 +0900

    optional binding added

--- ImagePicker2Photos/ViewController.swift
+++ ImagePicker2Photos/ViewController.swift
@@ -36,24 +36,28 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
         // ここからPhotosにアクセス
         let url: URL? = info[UIImagePickerControllerReferenceURL] as? URL // 画像の保存場所を取得
-        fetchAsset(from: url) // 画像の保存場所を取得して、fetchAssetに渡す
+        // 画像の保存場所を取得して、fetchAssetに渡して、PHAssetを取得
+        if let asset: PHAsset = fetchAsset(from: url), let location = asset.location {
+
+            locationLabel.text = "Location: latitude...\(location.coordinate.latitude), longtitude: \(location.coordinate.longitude)"
+        }
 
 
         dismiss(animated: true, completion: nil)
     }
 
     // 画像の保存場所をURL型でもらって、Photosにアクセスするメソッド
-    func fetchAsset(from url: URL?) {
+    func fetchAsset(from url: URL?) -> PHAsset? {
 
         // optional bingding
         guard let url: URL = url else {
 
-            return
+            return nil
         }
 
         // Photosで画像にアクセス。Locationとかの情報の入ったPHAssetを取得
         let asset: PHAsset? = PHAsset.fetchAssets(withALAssetURLs: [url], options: nil).firstObject // ここからLocationとかを取得するとよい
-        
+        return asset
     }
 }
 

396ca31063d0a540705439b96c895c6dde620f0e
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-09-03 18:26:36 +0900

    minimun imagepicker method added

--- ImagePicker2Photos/ViewController.swift
+++ ImagePicker2Photos/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
 
@@ -20,7 +20,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didSeletectedPresentImagePickerButton(_ sender: UIButton) {
-        
+
+        let controller = UIImagePickerController()
+        controller.sourceType = .photoLibrary
+        controller.delegate = self
+        self.present(controller, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+
+        let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        imageView.image = image
+        dismiss(animated: true, completion: nil)
     }
 }
 

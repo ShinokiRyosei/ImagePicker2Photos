c8911948013afb26457ebbf41873a934f3d4b42b
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-09-03 18:18:12 +0900

    initial commit

---
+++ ImagePicker2Photos/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ImagePicker2Photos
+//
+//  Created by ShinokiRyosei on 2017/09/03.
+//  Copyright © 2017年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+}
+
